88aa77b9e7542e480652b534e3af5b12a1cbabb3
Kafva <jonas@kafva.one>
2022-10-15 19:27:06 +0200

    -

--- client/Extensions/Color+hex.swift
+++ client/Extensions/Color+hex.swift
@@ -16,9 +16,10 @@ extension Color {
         if scanner.scanHexInt64(&hexNumber) {
           r  = CGFloat((hexNumber & 0xff000000) >> 24) / 255
           g  = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-          b  = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+          b  = CGFloat((hexNumber & 0x0000ff00) >> 8)  / 255
 
-          self.init( red: Double(r), green: Double(g), blue: Double(b) )
+          self.init(red: Double(r), green: Double(g), blue: Double(b))
+          print("hahah: \(r) \(g) \(b)")
           return;
         }
       }
