bea497414d55094f69019372a542c177591be9c5
Kafva <jonas@kafva.one>
2022-10-15 16:54:21 +0200

    Xcode template

---
+++ Podfile
@@ -0,0 +1,6 @@
+# vi: ft=ruby
+platform :ios, '14.0'
+
+target 'nass' do
+  use_frameworks!
+end
---
+++ nass/nassApp.swift
@@ -0,0 +1,17 @@
+//
+//  nassApp.swift
+//  nass
+//
+//  Created by Jonas on 2022-10-15.
+//
+
+import SwiftUI
+
+@main
+struct nassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
