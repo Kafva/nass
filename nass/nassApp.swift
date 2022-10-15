246058f39bf1615239d0f558f61c58a7c0693b25
Kafva <jonas@kafva.one>
2022-10-15 18:25:30 +0200

    Linting

--- nass/nassApp.swift
+++ nass/nassApp.swift
@@ -1,7 +1,18 @@
 import SwiftUI
 
-@main
-struct nassApp: App {
+// Search bar
+// Settings item
+//  Server location
+//  PSK
+// List of passwords/folders
+//  Page switches can use the same view (but with back-button)
+//
+// Password dialog (popup based on server response)
+// After one entry, any item can be pressed to reveal text or copy to clipboard
+
+
+// swiftlint:disable type_name
+@main struct nassApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
