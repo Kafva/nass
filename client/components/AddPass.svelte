97c5f4625830ef257b8ccb9cae2d0dd6722cd1d8
Kafva <jonas@kafva.one>
2022-10-24 23:24:12 +0200

    grid

--- client/components/AddPass.svelte
+++ client/components/AddPass.svelte
@@ -14,75 +14,62 @@ export let dialog: HTMLDialogElement;
 </script>
 
   <form method="dialog">
-    <div>
-      <label for="path">Path: </label>
-      <input type="text" name="path" required>
-    </div>
+      <div>
+        <label for="path">Path: </label>
+        <input type="text" name="path" required>
 
-    <div>
-      <label for="generate">Generate? </label>
-      <input type="checkbox" name="generate">
-    </div>
+        <label for="generate">Generate? </label>
+        <input type="checkbox" name="generate">
 
-    <div>
-      <label for="pass">Password: </label>
-      <input type="password" name="pass" required>
-    </div>
+        <label for="pass">Password: </label>
+        <input type="password" name="pass" required>
 
-    <div>
-      <label for="verify">Verify: </label>
-      <input type="password" name="verify" required>
-    </div>
+        <label for="verify">Verify: </label>
+        <input type="password" name="verify" required>
+      </div>
 
-    <div>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
       <button class="nf nf-fa-close"
               on:click="{() => ToggleDialog(dialog, cover, true)}" ></button>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
       <button class="nf nf-fa-arrow_right"
               on:click="{() => ToggleDialog(dialog, cover, true)}" ></button>
-    </div>
   </form>
 
 
 <style lang="scss">
 @use "../scss/vars";
 
-button.nf {
-  font-size: vars.$font_large;
-}
-
 form {
-  display: table;
 
   div {
-    display: table-row;
-
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
     label,input {
-      display: table-cell;
       margin-top: 10px;
     }
-    label {
-     padding-right: 10px;
+  }
+
+
+  button {
+    font-size: vars.$font_large;
+    color: vars.$white;
+    //width: 30px;
+    //height: 30px;
+
+    margin-top: 10px;
+    //padding: 10px 20px 10px 20px;
+    outline: 0;
+    border: 0;
+
+    &.nf-fa-close {
+      background-color: vars.$red;
     }
-
-    button {
-      display: table-cell;
-      color: vars.$white;
-      border: 0;
-      margin: 5px;
-      padding: 10px 20px 10px 20px;
-      outline: 0;
-
-      &.nf-fa-close {
-        background-color: vars.$red;
-      }
-      &.nf-fa-arrow_right {
-        background-color: vars.$green;
-      }
-
+    &.nf-fa-arrow_right {
+      background-color: vars.$green;
     }
 
   }
-
 }
 
 </style>
