3915e3efc315fb94ed8cc1c48116f1a482c1a10a
Kafva <jonas@kafva.one>
2022-10-26 07:44:17 +0200

    -

--- client/components/AddPass.svelte
+++ client/components/AddPass.svelte
@@ -12,29 +12,36 @@ export let cover: HTMLDivElement;
 export let dialog: HTMLDialogElement;
 
 let generatePass = true
+let passInput: HTMLInputElement;
+let verifyInput: HTMLInputElement;
 
 </script>
 
   <form method="dialog">
-      <!--
       <ul>
-        <li>The path name can be 1-255 characters from the set: [-_.@/a-zA-Z0-9]</li>
-        <li>'/' can be used to create folders</li>
+        <li>Latin alphanumerics and <code>[-_.@/]</code> are allowed in path names.</li>
+        <li><code>'/'</code> can be used to create folders.</li>
       </ul>
-      -->
+
       <div>
         <label for="path">Path:</label>
-        <input type="text" name="path" required>
+        <input spellcheck="false" type="text" name="path" required>
 
         <label for="generate">Generate:</label>
         <input type="checkbox" name="generate" bind:checked={generatePass}>
 
         {#if !generatePass}
           <label for="pass">Password:</label>
-          <input type="password" name="pass" required>
+          <input type="password" bind:this={passInput} name="pass" required>
 
           <label for="verify">Verify:</label>
-          <input type="password" name="verify" required>
+          <input type="password" bind:this={verifyInput} name="verify" required>
+          {#if passInput && verifyInput && 
+              passInput.value == verifyInput.value && passInput.value != "" }
+            <span class="nf nf-fa-check"/>
+          {:else}
+            <span class="nf nf-fa-close"/>
+          {/if}
         {/if}
       </div>
 
@@ -50,18 +57,46 @@ let generatePass = true
 form {
   text-align: center;
 
+  ul {
+    list-style-type: disc;
+    font-size: vars.$font_small;
+    display: inline-block;
+    width: 20vw;
+    text-align: left;
+    background-color: vars.$black;
+    opacity: 0.9;
+    border-radius: 0%;
+    border: 1px dotted;
+    border-color: vars.$white;
+    padding: 10px;
+    li {
+      margin-left: 20px;
+    }
+  }
+
   // Grid container
   div {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    label,input {
+    label,input,span {
       margin-top: 10px;
+      // Fade-in for explicit password entry
+      &:not(first-child) {
+        @include vars.fade-in;
+      }
+    }
+    span {
+      display: inline-block;
+      &.nf-fa-close {
+        color: vars.$red;
+      }
     }
     input {
       font-size: vars.$font_medium;
       padding: 5px 2px 5px 2px;
       @include vars.input-style;
     }
+    
   }
 
   button {
