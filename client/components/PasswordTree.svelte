ee298db88a95ba9b1167ce3ef538dd8e385e361c
Kafva <jonas@kafva.one>
2022-11-11 22:08:33 +0100

    -

--- client/components/PasswordTree.svelte
+++ client/components/PasswordTree.svelte
@@ -105,7 +105,7 @@
   {#if !isRoot }
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
-      class:dir="{!isLeaf}"
+      class="row"
       on:touchstart="{(e) => touch.start(e, deleteButton, showButton) }"
       on:touchmove="{(e) => touch.move(e, deleteButton, showButton) }"
       on:touchend="{(e) => {
@@ -124,7 +124,8 @@
       <!-- on:click() events are used on desktop and disabled in favor of
       ontouch* on mobile -->
       <span role="button"
-        class="{ isLeaf ? Config.passwordIcon : (open ? Config.dropdownOpen : Config.dropdownClosed)}"
+        class="{ isLeaf ? Config.passwordIcon : 
+          (open ? Config.dropdownOpen : Config.dropdownClosed)} row-icon"
         style:margin-left={marginLeft}
         on:click="{() => runIfNotMobile(handleMainClick) }"
       />
@@ -132,12 +133,14 @@
         {entry.name}
       </span>
 
-      {#if isLeaf}
-        <span role="button" class="{Config.showPassword} show-pass"
-              bind:this={showButton} on:click="{() => runIfNotMobile(handleGetPass, false) }"/>
-      {/if}
-      <span role="button" class="{Config.deleteIcon} delete-pass"
-            bind:this={deleteButton} on:click="{() => runIfNotMobile(handleDelPass)}" />
+      <div class="drawer">
+        {#if isLeaf}
+          <span role="button" class="{Config.showPassword} show-pass"
+                bind:this={showButton} on:click="{() => runIfNotMobile(handleGetPass, false) }"/>
+        {/if}
+        <span role="button" class="{Config.deleteIcon} delete-pass"
+              bind:this={deleteButton} on:click="{() => runIfNotMobile(handleDelPass)}" />
+      </div>
     </div>
   {/if}
 
@@ -152,78 +155,78 @@
 <style lang="scss">
   @use "../scss/vars";
 
-  div {
+  // Use a grid layout for each row
+  // ----------------------------------------------------------------
+  // | variable space | <span icon/> | <span name/> | <div drawer/> |
+  // ----------------------------------------------------------------
+  //
+  div.row {
     @include vars.fade-in(0.5s);
     font-size: vars.$font_medium;
     white-space: nowrap;
     padding: 4px 0 4px 0;
     margin: 2px 0 5px 0;
 
-    // Always show the border (translucently)
-    // to avoid geometry changes on :hover()
-    border-bottom: solid 1px;
-    border-color: rgba(0,0,0,0.0);
-
-    &.dir {
-      span {
-        display: inline;
-        width: fit-content;
-
-        &.name {
-          width: 50vw;
-        }
-      }
-    }
-
-    span.name {
-      text-overflow: ellipsis;
-      overflow: hidden;
-    }
-
-    span.nf {
-      text-overflow: clip;
-      overflow: hidden;
-      margin: 0 7px 0 7px;
-
-      // Hide all except the directory/key icon on the lhs
-      // by default.
-      &:not(:first-child) {
-        display: none;
-        @include vars.desktop-hover {
-          color: vars.$lilac;
-        }
-      }
-
-
-      // == Mobile ==
-      @include vars.mobile {
-        &.show-pass, &.delete-pass {
-          float: right;
-          //display: inline-block;
-          //white-space: nowrap;
-          overflow-x: hidden;
-          overflow-y: hidden;
-
-          padding: 2px 10px 2px 10px;
-          margin: 0px;
-          text-align: right;
-        }
-        &.show-pass {
-          background-color: vars.$green;
-        }
-        &.delete-pass {
-          background-color: vars.$red;
-        }
-      }
-    }
-
     @include vars.desktop-hover {
       border-color: vars.$lilac;
 
       // Display buttons on the direct child
       // when the parent element is hovered
-      & > span.nf:not(:first-child) {
+      div.drawer > span.nf:not(:first-child) {
         display: inline-block;
+      }
+    }
+
+    // Always show the border (translucently)
+    // to avoid geometry changes on :hover()
+    border-bottom: solid 1px;
+    border-color: rgba(0,0,0,0.0);
+
+    // == Icon and name ==
+    span {
+      display: inline;
+      width: fit-content;
+
+      &.name {
+        width: 50vw;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+      &.nf {
+        text-overflow: clip;
+        overflow: hidden;
+        margin: 0 7px 0 7px;
+
+        // Hide all except the directory/key icon on the lhs
+        // by default.
+        &:not(.row-icon) {
+          display: none;
+          @include vars.desktop-hover {
+            color: vars.$lilac;
+          }
+        }
+      }
+    }
+
+    div.drawer {
+      display: inline-block;
+      white-space: nowrap;
+      //overflow-x: hidden;
+      //overflow-y: hidden;
+      //padding: 2px 10px 2px 10px;
+      margin: 0px;
+      text-align: right;
+    }
+
+    // == Mobile ==
+    @include vars.mobile {
+      div.drawer {
+        span.show-pass {
+          background-color: vars.$green;
+        }
+        span.delete-pass {
+          background-color: vars.$red;
+        }
       }
     }
   }
