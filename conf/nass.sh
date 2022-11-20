539b47eb64f467c53aaa82427ea951353dae78b5
Kafva <jonas@kafva.one>
2022-11-20 17:37:21 +0100

    OpenRC service

---
+++ conf/nass.sh
@@ -0,0 +1,24 @@
+#!/sbin/openrc-run
+# vi: ft=sh
+# See: man openrc-run
+
+directory=/srv/nass
+output_log=/srv/nass/stdout.log
+error_log=/srv/nass/nass.log
+
+description="Password-store web API"
+procname="nass"
+pidfile=/run/nass.pid
+
+command_background=true
+command_user=$(id -u nass)
+command=/srv/nass/nass
+command_args="-c conf/nass.yml -u conf/users.yml"
+
+depend() {
+   need localmount
+   need net
+   need wireguard
+}
+
+
